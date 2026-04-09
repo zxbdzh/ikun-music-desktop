19c25193b7c015d1aae71eee95fd5a592b67ceef
Admin <1205039851@qq.com>
2026-04-09 17:38:02 +0800

    chore(config): 将仓库地址改为 zxbdzh/ikun-music-desktop

--- publish/utils/githubRelease.js
+++ publish/utils/githubRelease.js
@@ -29,7 +29,7 @@ const options = {
   draft: false,
   prerelease: false,
   repo: pkg.name,
-  owner: pkg.author,
+  owner: 'zxbdzh',
   endpoint: 'https://api.github.com', // for GitHub enterprise, use http(s)://hostname/api/v3
   auth: {
     token,
